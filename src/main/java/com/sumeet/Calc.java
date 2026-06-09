4d0b70d24921701cf85c93ad274f86007bde0a1c
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2026-06-09 16:08:42 +0530

    first maven project test

---
+++ src/main/java/com/sumeet/Calc.java
@@ -0,0 +1,12 @@
+package com.sumeet;
+
+public class Calc {
+    public double divide(int a, int b){
+        return (double)a/b;
+    }
+
+    public double multiply(int a, int b){
+        return a*b;
+    }
+    
+}
---
+++ src/test/java/com/sumeet/TestCalc.java
@@ -0,0 +1,18 @@
+package com.sumeet;
+
+// import org.junit.Test;
+// import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TestCalc {
+
+    @Test
+    public void testDivide(){
+        Calc myCalc= new Calc();
+        double result= myCalc.divide(10,5);
+        double expected= 2;
+
+        assertEquals(expected, result);
+    }
+    
+}
